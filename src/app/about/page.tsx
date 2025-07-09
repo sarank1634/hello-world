6a89f9653fab711eb78835bfbedbb20b603e973f
sarank1634 <saravanakumar1634@gmail.com>
2025-07-09 14:58:26 +0530

    fix few issues meta details

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
-export default function Blog() {
+export const metadata = {
+    title: 'About codeevolution',
+    description: 'code-eval',
+}
+
+
+export default function About() {
     return <h1>About</h1>;
 }
---
+++ src/app/revenue/page.tsx
@@ -0,0 +1,8 @@
+export default function Revenue() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Revenue</h1>
+      <p>This is the revenue page.</p>
+    </main>
+  );
+}
