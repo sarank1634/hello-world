b55bdd64f23a938593d967ea1c18a312bfca0ce7
sarank1634 <saravanakumar1634@gmail.com>
2025-07-09 13:32:57 +0530

    add layout style

---
+++ src/app/products/[productid]/layout.tsx
@@ -0,0 +1,13 @@
+export default function ProductdetailsLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <>
+        <div>
+           
+            {children}
+            <h2>Featured Products</h2>
+        </div>
+    </>
+}
